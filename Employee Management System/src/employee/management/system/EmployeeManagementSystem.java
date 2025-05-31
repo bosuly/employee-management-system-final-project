518f2055d29765d9755a71642d7b7d16d21b393d
bosuly <melendresulysses@g.cjc.edu.ph>
2025-05-30 18:43:22 -1200

    Add files via upload

---
+++ Employee Management System/src/employee/management/system/EmployeeManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package employee.management.system;
+
+/**
+ *
+ * @author USER
+ */
+public class EmployeeManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         head open = new head();
+        open.setVisible(true);
+        
+        
+    }
+    
+}
